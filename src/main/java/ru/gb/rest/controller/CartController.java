604a05e51f0034733d2d4a0507e434d7b174ee49
pezhe <pezhe@list.ru>
2022-03-07 18:16:03 +0300

    cart controller added again

--- src/main/java/ru/gb/rest/controller/CartController.java
+++ src/main/java/ru/gb/rest/controller/CartController.java
@@ -1,39 +1,39 @@
-//package ru.gb.rest.controller;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//import ru.gb.rest.dto.CartDto;
-//import ru.gb.rest.entity.Cart;
-//
-//import java.net.URI;
-//
-//@RestController
-//@RequiredArgsConstructor
-//@RequestMapping("/api/v1/cart")
-//public class CartController {
-//
-//    private final Cart cart;
-//
-//    @GetMapping
-//    public ResponseEntity<CartDto> handleGet() {
-//        return new ResponseEntity<>(new CartDto(cart.getProducts()), HttpStatus.OK);
-//    }
-//
-//    @DeleteMapping("/{productId}")
-//    @ResponseStatus(HttpStatus.NO_CONTENT)
-//    public void handleDelete(@PathVariable("productId") Long id) {
-//        cart.removeProduct(id);
-//    }
-//
-//    @PutMapping
-//    public ResponseEntity<?> handlePut(@RequestBody String id) {
-//        cart.addProduct(Long.parseLong(id));
-//        HttpHeaders httpHeaders = new HttpHeaders();
-//        httpHeaders.setLocation(URI.create("api/v1/cart/" + id));
-//        return new ResponseEntity<>(httpHeaders, HttpStatus.CREATED);
-//    }
-//
-//}
+package ru.gb.rest.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.gb.rest.dto.CartDto;
+import ru.gb.rest.entity.Cart;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/cart")
+public class CartController {
+
+    private final Cart cart;
+
+    @GetMapping
+    public ResponseEntity<CartDto> handleGet() {
+        return new ResponseEntity<>(new CartDto(cart.getProducts()), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{productId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void handleDelete(@PathVariable("productId") Long id) {
+        cart.removeProduct(id);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> handlePut(@RequestBody String id) {
+        cart.addProduct(Long.parseLong(id));
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(URI.create("api/v1/cart/" + id));
+        return new ResponseEntity<>(httpHeaders, HttpStatus.CREATED);
+    }
+
+}
--- src/main/java/ru/gb/rest/entity/Cart.java
+++ src/main/java/ru/gb/rest/entity/Cart.java
@@ -1,31 +1,35 @@
-//package ru.gb.rest.entity;
-//
-//import lombok.Getter;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Component;
-//import ru.gb.rest.service.ProductService;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Getter
-//@RequiredArgsConstructor
-//@Component
-//public class Cart {
-//
-//    private final List<Product> products = new ArrayList<>();
-//    private final ProductService productService;
-//
-//    public void addProduct(Long id) {
-//        if (products.stream().anyMatch((p) -> p.getId().equals(id))) return;
-//        products.add(productService.findById(id));
-//    }
-//
-//    public void removeProduct(Long id) {
-//        products.stream()
-//                .filter((p) -> p.getId().equals(id))
-//                .findAny()
-//                .ifPresent(products::remove);
-//    }
-//
-//}
+package ru.gb.rest.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.gb.rest.dao.ManufacturerDao;
+import ru.gb.rest.dto.mapper.ProductMapper;
+import ru.gb.rest.service.ProductService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+@Component
+public class Cart {
+
+    private final List<Product> products = new ArrayList<>();
+    private final ProductService productService;
+    private final ProductMapper productMapper;
+    private final ManufacturerDao manufacturerDao;
+
+    public void addProduct(Long id) {
+        if (products.stream().anyMatch((p) -> p.getId().equals(id))) return;
+        products.add(productMapper.toProduct(productService.findById(id), manufacturerDao));
+    }
+
+    public void removeProduct(Long id) {
+        products.stream()
+                .filter((p) -> p.getId().equals(id))
+                .findAny()
+                .ifPresent(products::remove);
+    }
+
+}
